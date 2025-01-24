f44ed94d5496db2e216a680a656c8a61b910f506
Fredaza99 <143113719+Fredaza99@users.noreply.github.com>
2025-01-24 10:01:36 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.getElementById("menu-toggle");
+    const menuOverlay = document.getElementById("menu-overlay");
+    const closeIcon = document.getElementById("close-icon");
+
+    // Abrir e fechar o menu ao clicar no botão toggle
+    menuToggle.addEventListener("click", () => {
+        menuOverlay.classList.toggle("active");
+    });
+
+    // Fechar o menu ao clicar no botão de fechar
+    closeIcon.addEventListener("click", () => {
+        menuOverlay.classList.remove("active");
+    });
+
+    // Fechar o menu ao clicar em qualquer link dentro do menu
+    const menuLinks = document.querySelectorAll(".menu-links-mobile a");
+    menuLinks.forEach(link => {
+        link.addEventListener("click", () => {
+            menuOverlay.classList.remove("active");
+        });
+    });
+});
+
+
+
 document.querySelector('.cta-button').addEventListener('click', () => {
-    window.location.href = 'https://wa.me/SEUNUMERO';
+    window.location.href = 'https://wa.me/5541991717037';
+});
+
+window.addEventListener("scroll", () => {
+    const scrollOffset = window.scrollY * 0.3; // Velocidade do paralaxe
+    canvas.style.transform = `translateY(${scrollOffset}px)`;
 });
 
 
@@ -14,6 +45,7 @@ document.addEventListener("DOMContentLoaded", () => {
         skill.style.strokeDashoffset = offset; // Atualiza o progresso
     });
 });
+
 
 
 
